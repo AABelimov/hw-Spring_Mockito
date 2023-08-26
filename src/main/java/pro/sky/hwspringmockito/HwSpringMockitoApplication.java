c863596daf7216c6f261403c01bdf8c52202afa2
AABelimov <belimov98@gmail.com>
2023-08-26 15:07:08 +0300

    Initial commit

---
+++ src/main/java/pro/sky/hwspringmockito/HwSpringMockitoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hwspringmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSpringMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/hwspringmockito/HwSpringMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hwspringmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
